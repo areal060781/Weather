9a601dd3d0dce4b30a069db862bffb9a25de903f
Aida Real <areal060781@gmail.com>
2018-07-14 23:43:58 -0500

    Ajax request with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'df42609430782b10537fc7df2a392b16';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather(){
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
     return {
         type: FETCH_WEATHER,
+        payload: request
     }
 }
